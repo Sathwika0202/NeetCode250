79b4cda0a7952b8fea9f0c8d76e20ecc110004a4
Sathwika0202 <139248088+Sathwika0202@users.noreply.github.com>
2026-06-16 07:17:11 -0700

    Time: 0 ms (100%), Space: 20.4 MB (60.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            find = target - nums[i]
-            for j in range(len(nums)):
-                if(nums[j]==find and (i!=j)):
-                    return [i,j]
+        # for i in range(len(nums)):
+        #     find = target - nums[i]
+        #     for j in range(len(nums)):
+        #         if(nums[j]==find and (i!=j)):
+        #             return [i,j]
 
-        return []                             
+        # return []       
+
+        dic = {}
+        for i,num in enumerate(nums):
+
+            find = target - num
+
+            if find in dic:
+                return [dic[find],i]
+
+            dic[num] = i    
+        return []         
